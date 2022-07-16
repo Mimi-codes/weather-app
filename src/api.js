f63c374b069adeed28c79fe6eaf9ac1aa5c11e83
Tomi <mariamismael904@gmail.com>
2022-07-16 23:32:30 +0100

    ...

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export  const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fc0335fd74msh39ffffd67147753p131b81jsnf2ecb5f037af',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
